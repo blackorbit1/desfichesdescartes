2885197d19c4df133f395a61089495a87046c62c
Justin-Fouquet <37479575+Justin-Fouquet@users.noreply.github.com>
2018-07-26 17:06:09 +0200

    Delete bdd.php

--- bdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-try{
-    //$bdd = new PDO('mysql:host=db719518578.db.1and1.com;dbname=db719518578;charset=utf8', 'dbo719518578', '-1Petitbulle');
-    $bdd = new PDO('mysql:host=localhost;dbname=desfichesdescartes;charset=utf8', 'root', 'root');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-?>
