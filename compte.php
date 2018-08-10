5dbd706471f25661d6439b6ab9d74a0f6d67ce95
blackorbit1 <38140797+blackorbit1@users.noreply.github.com>
2018-08-10 20:07:18 +0200

    Page fonctionnelle sauf pour changer l'avatar

--- compte.php
+++ compte.php
@@ -1,21 +1,84 @@
 <?php
 session_cache_limiter('private_no_expire, must-revalidate');
 session_start();
+include_once("bdd.php");
+
+
+
+print_r($_POST);
+print_r($_SESSION);
+print("Erreur : " . (int) $erreur);
+
 include_once("test_session.php");
 include_once("navigateur.php");
 include_once("compteur_vues.php");
 include_once("logs.php");
 
-$navigateur = get_browsername();
 
 if ($id_session != "hacker_du_93" && $id_session != "user"){
     $action = "A tenté d'acceder à la page de compte sans etre admin ou utilisateur inscrit";
     logs(isset($_SESSION["pseudo"])?$_SESSION["pseudo"]:"inconnu", $action);
+    /*
     header("Location: index.php");
-    exit; //ou die, c'est pareil
+    exit; //ou die, c'est pareil*/
 } else {
-    $action = "Acces à lapage de compte";
+    $action = "Acces à la page de compte";
     logs(isset($_SESSION["pseudo"])?$_SESSION["pseudo"]:"inconnu", $action);
+    /// --- CODE TRAITEMENT DES REQUETES --- /// (doit etre au début pour identification de l'utilisateur)
+    $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+    if(isset($_POST["pseudo"]) && isset($_POST["id"])){
+        if($_POST["pseudo"] == ""){
+            ?><div class='deleted'>La fourchette sert à manger avec convenance</div><br/><?php
+            logs($_SESSION["pseudo"], "A tenté de forcer le changement de pseudo avec un pseudo vide");
+        } else {
+            $req = $bdd->prepare('UPDATE ventilateur SET
+                                                    old_u_u_s_e_r = :old_u_u_s_e_r
+                                                WHERE id_number = :id ');
+            $erreur = $req->execute(array(
+                "old_u_u_s_e_r" => $_POST["pseudo"],
+                "id" => $_POST["id"]
+            ));
+            $_SESSION['pseudo'] = $_POST["pseudo"];
+            $user_session = htmlentities($_SESSION['pseudo'], ENT_QUOTES);
+        }
+    } elseif (isset($_POST["mdp"]) && isset($_POST["id"])) {
+        if($_POST["mdp"] == ""){
+            ?><div class='deleted'>Please put something dans le champ de mot de passe</div><br/><?php
+            logs($_SESSION["pseudo"], "A tenté de forcer le changement de mot de passe avec un mot de passe vide");
+        } else {
+            $req = $bdd->prepare('UPDATE ventilateur SET
+                                                    old_m_m_d_p__ = :old_m_m_d_p__
+                                                WHERE id_number = :id ');
+            $erreur = $req->execute(array(
+                "old_m_m_d_p__" => md5($_POST["mdp"]),
+                "id" => $_POST["id"]
+            ));
+            $_SESSION['mdp'] = md5($_POST["mdp"]);
+            $mdp_session = $_SESSION['mdp'];
+
+        }
+    } elseif (isset($_POST["pseudo_discord"]) && isset($_POST["id"])) {
+        if($_POST["pseudo_discord"] == ""){
+            ?><div class='deleted'>YnJhdm8gw6AgdG9pLCB0cm91dmUgbGEgcHJvY2hhaW5lIMOpbmlnbWU=</div><br/><?php
+            logs($_SESSION["pseudo"], "A tenté de forcer le changement du pseudo discord avec un pseudo vide");
+        } else {
+            $req = $bdd->prepare('UPDATE ventilateur SET
+                                                    d_isc0rd_nomination = :d_isc0rd_nomination
+                                                WHERE id_number = :id ');
+            $erreur = $req->execute(array(
+                "d_isc0rd_nomination" => htmlspecialchars($_POST["pseudo_discord"] . "#" . $_POST["num_discord"]),
+                "id" => $_POST["id"]
+            ));
+        }
+    } elseif (isset($_POST["niveau"]) && isset($_POST["id"])) {
+        $req = $bdd->prepare('UPDATE ventilateur SET
+                                                mivault = :mivault
+                                            WHERE id_number = :id ');
+        $erreur = $req->execute(array(
+            "mivault" => htmlspecialchars($_POST["niveau"]),
+            "id" => $_POST["id"]
+        ));
+    }
 }
 
 //$bdd->exec('INSERT INTO logs_admins(admin, date, IP, navigateur, action) VALUES("'. $user_session .'" , NOW(), "'. $_SERVER["REMOTE_ADDR"] .'", "'. $navigateur .'" , "'. $action .'")');
@@ -124,7 +187,8 @@ $ELEMENTSPARPAGE = 48; //  <<<<< CHANGER LE SYSTEME
 
 
                         <?php
-                            $req = $bdd->prepare("SELECT id_number, id, old_u_u_s_e_r, atavar_001, mivault, cadecount FROM ventilateur WHERE old_u_u_s_e_r = :old_u_u_s_e_r and old_m_m_d_p__ = :old_m_m_d_p__");
+                            
+                            $req = $bdd->prepare("SELECT id_number, id, old_u_u_s_e_r, atavar_001, mivault, cadecount, d_isc0rd_nomination FROM ventilateur WHERE old_u_u_s_e_r = :old_u_u_s_e_r and old_m_m_d_p__ = :old_m_m_d_p__");
                             //print("aaaaaaaaaaaa". (int) $req ."aaaaaaaaaaaaa");
                             $req->execute(array("old_u_u_s_e_r" => $_SESSION['pseudo'],
                                                 "old_m_m_d_p__" => $_SESSION['mdp']));
@@ -137,28 +201,54 @@ $ELEMENTSPARPAGE = 48; //  <<<<< CHANGER LE SYSTEME
                             <tr>
                                 <td style="width: 220px;" valign="top"> <!-- Avatar + ID compte -->
                                     <div class="cadre_modifier_avatar">
-                                        <img src="uploads/<?php print($donnees["atavar_001"]); ?>" alt="sabeug2partou" />
+                                        <img src="<?php print($donnees["atavar_001"]); ?>" alt="sabeug2partou" style="width: 100%;height: 100%;" />
                                         <div class="modifier_avatar"><br>Modifier</div>
                                     </div>
 
                                     <div class="id_compte">ID compte: <strong><?php print($donnees["id_number"]); ?></strong></div>
 
                                 </td>
-                                <td valign="top"> <!-- Reste -->
-                                Pseudo : <input style="width: 500px;" type="text" name="pseudo" value=<?php htmlspecialchars(print("'" . $donnees["old_u_u_s_e_r"] . "'")) ?>/>
-                                <br><div class="bouton" onClick="changerMotDePasse()">Changer de mot de passe</div>
-                                <!-- <input style="width: 500px;" type="text" name="nom" value=<?php htmlspecialchars(print("'" . $donnees["old_u_u_s_e_r"] . "'")) ?>/> -->
-                                <br>Niveau : 
-                                <select name="niveau">
-                                    <option value=<?php print($donnees["mivault"]) ?> id="to_hide"><?php print($donnees["mivault"]) ?></option>
-                                    <option value="L1_math_info">L1</option>
-                                    <option value="L2_math">L2 (maths)</option>
-                                    <option value="L2_info">L2 (info)</option>
-                                    <option value="L2_math_info">L2 (double licence)</option>
-                                    <option value="L3_math">L3 (maths)</option>
-                                    <option value="L3_info">L3 (info)</option>
-                                    <option value="L3_math_info">L3 (double licence)</option>
-                                </select>
+                                <td valign="top" style="padding-left: 20px;"> <!-- Reste -->
+                                    <form action="" method="post">
+                                        <label for="pseudo">Pseudo : </label>
+                                        <input id="pseudo" style="width: 200px;" type="text" name="pseudo" value=<?php htmlspecialchars(print("'" . $donnees["old_u_u_s_e_r"] . "'")) ?> required/>
+                                        <input type="hidden" name="id" value="<?php print($donnees["id_number"]); ?>"/>
+                                        <input type="submit" value="Changer" />
+                                    </form>
+                                    <form action="" method="post">
+                                        <label for="mdp">Mot de passe : </label>
+                                        <input id="mdp" style="width: 200px;" type="password" name="mdp" required/>
+                                        <input type="hidden" name="id" value="<?php print($donnees["id_number"]); ?>"/>
+                                        <input type="submit" value="Changer" />
+                                    </form>
+
+                                    <br>
+
+                                    <form action="" method="post">
+                                        <label for="pseudo_discord">Pseudo discord : </label>
+                                        <?php error_reporting(0); ?>
+                                        <input id="pseudo_discord" style="width: 150px;" type="text" name="pseudo_discord" value=<?php htmlspecialchars(print("'" . explode("#", $donnees["d_isc0rd_nomination"])[0] . "'")) ?> required/>#<input style="width: 50px;" type="text" name="num_discord" value=<?php htmlspecialchars(print("'" . end(explode("#", $donnees["d_isc0rd_nomination"])) . "'")) ?> required/>
+                                        <?php error_reporting(E_ALL); ?>
+                                        <input type="hidden" name="id" value="<?php print($donnees["id_number"]); ?>"/>
+                                        <input type="submit" value="Changer" />
+                                    </form>
+                                    <form action="" method="post">
+                                        <label for="niveau">Niveau : </label>
+                                        <select id="niveau" name="niveau" >
+                                            <option value=<?php print($donnees["mivault"]) ?> id="to_hide"><?php print($donnees["mivault"]) ?></option>
+                                            <option value="L1_math_info">L1</option>
+                                            <option value="L2_math">L2 (maths)</option>
+                                            <option value="L2_info">L2 (info)</option>
+                                            <option value="L2_math_info">L2 (double licence)</option>
+                                            <option value="L3_math">L3 (maths)</option>
+                                            <option value="L3_info">L3 (info)</option>
+                                            <option value="L3_math_info">L3 (double licence)</option>
+                                        </select>
+                                        <input type="hidden" name="id" value="<?php print($donnees["id_number"]); ?>"/>
+                                        <input type="submit" value="Changer" />
+                                    </form>
+                                    <!-- <input style="width: 500px;" type="text" name="nom" value=<?php htmlspecialchars(print("'" . $donnees["old_u_u_s_e_r"] . "'")) ?>/> -->
+
 
 
                                 
