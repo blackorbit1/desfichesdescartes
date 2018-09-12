455e681513b01bb3a681159d78e4025a63c39907
blackorbit1 <38140797+blackorbit1@users.noreply.github.com>
2018-09-12 12:00:48 +0200

    correction d'une possible faille

--- auto_log_on_test_server.php
+++ auto_log_on_test_server.php
@@ -10,13 +10,14 @@ if(strstr(getcwd(), "/htdocs/test/desfichesdescartes") || strstr(getcwd(), "\www
             <input type="submit" value="Passer en mode admin" class="bouton" style="background-color: orange;" />
         </form>
     <?php
+    if(isset($_POST["auto_pseudo"]) || isset($_POST["auto_mdp"])) {
+        $_SESSION['pseudo'] = htmlentities($_POST['auto_pseudo'], ENT_QUOTES);
+        $_SESSION['mdp'] = htmlentities($_POST['auto_mdp'], ENT_QUOTES);
+        $id_session = "hacker_du_93";
+        $user_session = "blackorbit";
+    }
 } 
-if(isset($_POST["auto_pseudo"]) || isset($_POST["auto_mdp"])) {
-    $_SESSION['pseudo'] = htmlentities($_POST['auto_pseudo'], ENT_QUOTES);
-    $_SESSION['mdp'] = htmlentities($_POST['auto_mdp'], ENT_QUOTES);
-    $id_session = "hacker_du_93";
-    $user_session = "blackorbit";
-}
+
 /*
 print("<p>Le dossier actuel: ".  $_SESSION['pseudo'] ."</p>");
 print("<p>Le dossier actuel: ".  $_SESSION['mdp'] ."</p>");
