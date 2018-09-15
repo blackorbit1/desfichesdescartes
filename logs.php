409d2d0f4d836fadd992597147c9f27ead4504c9
blackorbit1 <38140797+blackorbit1@users.noreply.github.com>
2018-09-15 18:31:37 +0200

    transfert crawler_detector() vers bot_control.php

--- logs.php
+++ logs.php
@@ -1,37 +1,15 @@
 <?php
 include_once("navigateur.php");
 
-function crawler_detector() {
-    // User lowercase string for comparison.
-    $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
-    // A list of some common words used only for bots and crawlers.
-    $bot_identifiers = array(
-        'bot',
-        'slurp',
-        'crawler',
-        'spider',
-        'curl',
-        'facebook',
-        'fetch',
-    );
 
-    // See if one of the identifiers is in the UA string.
-    foreach ($bot_identifiers as $identifier) {
-        if (strpos($user_agent, $identifier) !== FALSE) {
-            return TRUE;
-        }
-    }
-    $server_host = $_SERVER["HTTP_HOST"];
-    $host_name = gethostname();
-    if(preg_match("#(?:googlebot|google\.com|slurp|crawler|w3af|havij|baidu|bingbot|feedfetcher|exabot|yandex|FacebookExternalHit|Discordbot|xml\-sitemaps|uCrawler|DuckDuckGo|SurdotlyBot)#iu", $server_host.$host_name)){
-        return TRUE;
-    }
-    return FALSE;
-}
 
 function logs($auteur, $action){
-    include("bdd.php");
-    if(crawler_detector()){
+    include_once("bdd.php");
+
+    $req = $bdd->prepare("SELECT id FROM bots WHERE ip = :ip");
+    $req->execute(array("ip" => $_SERVER["REMOTE_ADDR"]));
+
+    if($req->rowCount()){
         $auteur = "bot";
     }
     $req = $bdd->prepare('INSERT INTO logs_admins(  admin,
