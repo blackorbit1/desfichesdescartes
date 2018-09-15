dfe0b656e2fd0c72fd21e50ae91d6275080f29fc
blackorbit1 <38140797+blackorbit1@users.noreply.github.com>
2018-09-15 20:27:40 +0200

    test affichage adresse IP

--- test.php
+++ test.php
@@ -1,11 +1,3 @@
 <?php
-    ob_start();
-    system('ipconfig /all');
-    $mycom=ob_get_contents();
-    ob_clean();
-      
-    $findme = 'physique';
-    $pmac = strpos($mycom, $findme);
-    $mac=substr($mycom,($pmac+32),20);
-    echo $mac;
+    echo $_SERVER['REMOTE_ADDR'];
 ?>
